ed1ab1c081f923e9eacddcb497307f0158985237
BLahari19 <laharibodduluri10@gmail.com>
2025-08-12 11:36:42 +0530

    Initial commit

---
+++ src/main/java/com/klu/CicdBackend2Application.java
@@ -0,0 +1,13 @@
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdBackend2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdBackend2Application.class, args);
+	}
+
+}
