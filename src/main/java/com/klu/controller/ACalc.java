f7488bb393143f412f67ccf267a0af834281df8e
BLahari19 <laharibodduluri10@gmail.com>
2025-08-26 10:54:13 +0530

    Update ACalc.java

--- src/main/java/com/klu/controller/ACalc.java
+++ src/main/java/com/klu/controller/ACalc.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/calc1")   // ✅ base path added
+@RequestMapping("/calc")   // ✅ base path added
 public class ACalc {
 
     @GetMapping("add/{A}/{B}")
