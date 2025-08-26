d6f73b78bb15930a33cb470b5f815299a8dc9358
BLahari19 <laharibodduluri10@gmail.com>
2025-08-26 10:25:04 +0530

    CicdBackend2Application.java

--- src/main/java/com/klu/CicdBackend2Application.java
+++ src/main/java/com/klu/CicdBackend2Application.java
@@ -2,12 +2,19 @@ package com.klu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CicdBackend2Application {
+public class CicdBackend2Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CicdBackend2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CicdBackend2Application.class, args);
+    }
 
+    // This is required for WAR deployment in external Tomcat
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CicdBackend2Application.class);
+    }
 }
